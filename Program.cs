1556354f560808ada501e3b3f5a847dcd1a08772
Ekaterina Chabanova <106866835+medochecheck@users.noreply.github.com>
2024-11-14 11:56:07 +0300

    Учет дробных чисел при вводе

--- Program.cs
+++ Program.cs
@@ -1,25 +1,94 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main()
     {
+        double num1, num2;
+
+        // Только целые числа
+
+        /*
         Console.Write("Введите первое число: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
+        while (!double.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            Console.Write("Введите первое число: ");
+        }
 
+        // Ввод второго числа с проверкой
         Console.Write("Введите второе число: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        while (!double.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            Console.Write("Введите второе число: ");
+        }
+        */
 
-        double sum = num1 + num2;
-        double difference = num1 - num2;
-        double product = num1 * num2;
 
-        string division = num2 != 0 ? (num1 / num2).ToString() : "Ошибка: деление на ноль невозможно";
+        // С учетом обработки десятичных дробей
+
+        Console.Write("Введите первое число (дробная часть отделяется точкой или запятой): ");
+        while (!TryParseDouble(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            Console.Write("Введите первое число: ");
+        }
+
+
+        // Ввод второго числа с проверкой
+
+        Console.Write("Введите второе число (дробная часть отделяется точкой или запятой): ");
+        while (!TryParseDouble(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            Console.Write("Введите второе число: ");
+        }
+
+
+        // Выполнение операций через отдельные функции
+
+        double sum = Add(num1, num2);
+        double difference = Subtract(num1, num2);
+        double product = Multiply(num1, num2);
+        string division = Divide(num1, num2);
+
+
+        // Функция сложения
+
+        double Add(double a, double b) { return a + b; }
+
+        // Функция вычитания
+
+        double Subtract(double a, double b) { return a - b; }
+
+        // Функция умножения
+
+        double Multiply(double a, double b) { return a * b; }
+
+        // Функция деления с проверкой на ноль
+
+        string Divide(double a, double b)
+        { return b != 0 ? (a / b).ToString(CultureInfo.InvariantCulture) : "Ошибка: деление на ноль невозможно"; }
+
 
         Console.WriteLine("\nРезультаты операций:");
-        Console.WriteLine("Сложение: " + sum);
-        Console.WriteLine("Вычитание: " + difference);
-        Console.WriteLine("Умножение: " + product);
+        Console.WriteLine("Сложение: " + sum.ToString(CultureInfo.InvariantCulture));
+        Console.WriteLine("Вычитание: " + difference.ToString(CultureInfo.InvariantCulture));
+        Console.WriteLine("Умножение: " + product.ToString(CultureInfo.InvariantCulture));
         Console.WriteLine("Деление: " + division);
+
+
+        // Метод для обработки ввода с учетом разделителя точки и запятой
+
+        bool TryParseDouble(string input, out double result)
+        {
+            if (double.TryParse(input, NumberStyles.Any, CultureInfo.CurrentCulture, out result))
+            {
+                return true;
+            }
+            return double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out result);
+        }
     }
 }
